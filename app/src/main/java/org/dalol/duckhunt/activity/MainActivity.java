6b14283a9f4a9f6e3791075c8640c34951a99ef6
Filippo <filippo.ash@moneysupermarket.com>
2016-03-25 15:35:12 +0000

    Added a documentation

--- app/src/main/java/org/dalol/duckhunt/activity/MainActivity.java
+++ app/src/main/java/org/dalol/duckhunt/activity/MainActivity.java
@@ -6,7 +6,11 @@ import android.view.WindowManager;
 
 import org.dalol.duckhunt.game.Game;
 
-
+/**
+ * @author Filippo Engidashet <filippo.eng@gmail.com>
+ * @version 1.0.0
+ * @since 8/19/2015
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
